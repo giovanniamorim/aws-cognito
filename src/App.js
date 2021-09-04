c22f6504ba760519fb9770c102dc496f9060fef6
Giovanni Amorim <giovanni.amorim.ti@gmail.com>
2021-09-04 20:03:11 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import React, { Component }  from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Authenticator } from 'aws-amplify-react';
+import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui/dist/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  componentDidMount(){
+    Amplify.configure({
+      mandatorySignin: true,
+      region: 'sa-east-1',
+      userPoolId: 'sa-east-1_GnUdtFICL',
+      identityPoolId: 'sa-east-1:43fbe5cb-ee35-4673-b232-0f04994c330f',
+      userPoolWebClientId: '3s1ju6vbkitvp90uaadm6kstil'
+    })
+
+  }
+  render() {
+    return (
+      <div>
+        <Authenticator />
+      </div>
+    )
+  }
+
 }
 
-export default App;
